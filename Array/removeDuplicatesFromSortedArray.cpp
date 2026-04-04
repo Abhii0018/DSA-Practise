af242e1c34c39c6f8dbacaf6c15bf88f0b6b2b3c
Abhishek Kumar <abhisheksah018@gmail.com>
2026-04-04 11:03:22 +0530

    Add remove duplicates from sorted array

---
+++ Array/removeDuplicatesFromSortedArray.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        
+        int i = 0;
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {1, 1, 2, 2, 3, 4, 4, 5};
+    int k = sol.removeDuplicates(nums);
+    
+    cout << "Array after removing duplicates: ";
+    for (int i = 0; i < k; i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
