64c253f3da3fcdfb3ac5e0fb289259c647d74a87
Abhishek Kumar <abhisheksah018@gmail.com>
2026-04-21 09:17:47 +0530

    solves fist and last elemnt usig. binary search

---
+++ Binary Search/findFirst&LastElem.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int nums[] = {5,7,7,8,8,10};
+    int  target = 8;
+
+// Output: [3,4]
+// Example 2:
+
+// Inputnums = [5,7,7,8,8,10], target = 6
+// Output: [-1,-1]
+
+
+
+
+int len=sizeof(nums)/sizeof(nums[0]);
+
+
+    return 0;
+}
